e023ca08b19b96197da6dacf2d5a71313cd1d4e4
Przemyslaw Markiewicz <kacz.pacz@gmail.com>
2018-11-05 22:44:22 +0100

    Shader class fstream add, fix some minor things in color class

--- OPENGL_triangles_object/OPENGL_triangles_object/Source.cpp
+++ OPENGL_triangles_object/OPENGL_triangles_object/Source.cpp
@@ -1,45 +1,13 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm.hpp>
-#include <vector>
 #include <iostream>
-#include "vertex.hpp"
-#include "color.hpp"
+#include "vertices.hpp"
 #include "shader.hpp"
 
 
-//dorobic system parsowania wspolrzednych
-//poprawic parser kolorow
-//dorobic klase buffor
-//zrobic interfejs buffor<=>trojkat
-/*
-	void pix2double()
-	{
-		for (auto &vertice : vertices) {
-			vertice.x = 1.0f / SCR_WIDTH * 2.0f * vertice.x;
-			vertice.y = 1.0f / SCR_HEIGHT * 2.0f * vertice.y;
-		}
-	}
-*/
-
 unsigned SCR_WIDTH = 800;
 unsigned SCR_HEIGHT = 600;
-
-class triangle {
-private:
-	color _color;
-	shader _shader;
-public:
-	triangle()
-	{
-		_color = COLOR_BLUE;
-		_shader = shader(_color);
-	}
-	void draw()
-	{
-		_shader.run();
-	}
-};
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -67,30 +35,13 @@ int main(void)
 		return -1;
 	}
 
-	triangle my_triangle;
 	
-
-	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f
-	};
-
-	unsigned VBO, VAO;
-	glGenBuffers(1, &VBO);
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
 
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
 		glClearColor(0.5f, 0.4f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		my_triangle.draw();
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
--- OPENGL_triangles_object/OPENGL_triangles_object/shader.hpp
+++ OPENGL_triangles_object/OPENGL_triangles_object/shader.hpp
@@ -4,121 +4,102 @@
 #include <fstream>
 #include <string>
 #include <sstream>
-
-//file streaming
+#include <algorithm>
 
 class shader
 {
 private:
-	std::string _vertexShader;
-	std::string _fragmentShader;
 	unsigned vertexShader;
 	unsigned fragmentShader;
 	unsigned shaderProgram;
-	void compile();
+	void compile(const char *c_vertexcode, const char *c_fragmentcode);
 public:
-	void run();
-	shader() {}
-	shader(const std::string vertexpath, const std::string fragmentpath);
+	shader() : vertexShader(0), fragmentShader(0), shaderProgram(0) {};
+	shader(const std::string &vertexpath, const std::string &fragmentpath);
 	shader(const shader &other);
 	shader& operator=(const shader &other);
 	shader(shader &&other) noexcept;
 	shader& operator=(shader &&other) noexcept;
 	~shader();	
+	void run() const;
+	void link_shader(const std::string &vertexpath, const std::string &fragmentpath);
 };
 
-shader::shader(color _color)
+shader::shader(const std::string &vertexpath, const std::string &fragmentpath)
 {
-
-	_vertexShader = "#version 330 core\n"
-		"layout (location = 0) in vec3 aPos;\n"
-		"void main()\n"
-		"{\n"
-		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-		"}\0";
-
-	_fragmentShader = "#version 330 core\n"
-		"out vec4 FragColor;\n"
-		"void main()\n"
-		"{\n"
-		"   FragColor = vec4("
-		+ _color.get_buffer() +
-		");\n"
-		"}\n\0";
-
-	compile();
+	std::fstream vertexfile(vertexpath, std::fstream::in);
+	std::fstream fragmentfile(fragmentpath, std::fstream::in);
+	std::stringstream vertexstream;
+	std::stringstream fragmentstream;
+	std::string vertexcode;
+	std::string fragmentcode;
+	vertexfile.exceptions(std::fstream::badbit | std::fstream::failbit);
+	fragmentfile.exceptions(std::fstream::badbit | std::fstream::failbit);
+	vertexstream << vertexfile.rdbuf();
+	fragmentstream << fragmentfile.rdbuf();
+	vertexcode = vertexstream.str();
+	fragmentcode = fragmentstream.str();
+	const char *c_vertexcode = vertexcode.c_str();
+	const char *c_fragmentcode = fragmentcode.c_str();
+	compile(c_vertexcode, c_fragmentcode);
+	delete c_vertexcode;
+	delete c_fragmentcode;
 }
+
 
 shader::shader(const shader &other)
 {
-	_vertexShader = other._vertexShader;
-	_fragmentShader = other._fragmentShader;
-	compile();
+	shaderProgram = other.shaderProgram;
 }
 
 shader& shader::operator=(const shader &other)
 {
-	_vertexShader.clear();
-	_fragmentShader.clear();
-	_vertexShader = other._vertexShader;
-	_fragmentShader = other._fragmentShader;
-	compile();
+	return *this = shader(other);
 }
 
 shader::shader(shader &&other) noexcept
 {
-	_vertexShader = other._vertexShader;
-	_fragmentShader = other._fragmentShader;
-	other._vertexShader.clear();
-	other._fragmentShader.clear();
-	compile();
+	shaderProgram = std::exchange(other.shaderProgram, 0);
 }
 
 shader& shader::operator=(shader &&other) noexcept
 {
-	_vertexShader.clear();
-	_fragmentShader.clear();
-	_vertexShader = other._vertexShader;
-	_fragmentShader = other._fragmentShader;
-	other._vertexShader.clear();
-	other._fragmentShader.clear();
-	compile();
+	std::swap(shaderProgram, other.shaderProgram);
 	return *this;
 }
 
 shader::~shader()
 {
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
+	glDeleteShader(shaderProgram);
 }
 
-void shader::compile()
+void shader::compile(const char *c_vertexcode, const char *c_fragmentcode)
 {
-	char *vertexShaderSource = new char[_vertexShader.length()];
-	char *fragmentShaderSource = new char[_fragmentShader.length()];
-
-	std::strcpy(vertexShaderSource, _vertexShader.c_str());
-	std::strcpy(fragmentShaderSource, _fragmentShader.c_str());
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 7, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+	glShaderSource(vertexShader, 1, &c_vertexcode, nullptr);
 	glCompileShader(vertexShader);
 	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+	glShaderSource(fragmentShader, 1, &c_fragmentcode, nullptr);
 	glCompileShader(fragmentShader);
 	shaderProgram = glCreateProgram();
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
 	glLinkProgram(shaderProgram);
-	delete vertexShaderSource;
-	delete fragmentShaderSource;
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 }
 
-void shader::run()
+void shader::run() const
 {
 	glUseProgram(shaderProgram);
 	glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
+void shader::link_shader(const std::string &vertexpath, const std::string &fragmentpath)
+{
+	*this = shader(vertexpath, fragmentpath);
 }
 
 #endif

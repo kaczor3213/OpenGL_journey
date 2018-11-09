8216bf86e5cbfafbc559b95e31d1d8ec34b8ab02
Moew34 <Moew34@DESKTOP-PMFMPIR>
2018-11-09 16:15:46 +0100

    Changes made in point.cpp

--- OpenGL_triangle/cmake-build-debug/CMakeFiles/OpenGL_triangle.dir/DependInfo.cmake
+++ OpenGL_triangle/cmake-build-debug/CMakeFiles/OpenGL_triangle.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "F:/STUDIES/CPP/OpenGL_journey/OpenGL_triangle/main.cpp" "F:/STUDIES/CPP/OpenGL_journey/OpenGL_triangle/cmake-build-debug/CMakeFiles/OpenGL_triangle.dir/main.cpp.obj"
+  "D:/Git/OpenGL_journey/OpenGL_triangle/main.cpp" "D:/Git/OpenGL_journey/OpenGL_triangle/cmake-build-debug/CMakeFiles/OpenGL_triangle.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 

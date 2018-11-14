68e19783e27e0455bb9e464bd70a4101d66d2ab9
Przemyslaw Markiewicz <kacz.pacz@gmail.com>
2018-11-14 15:08:26 +0100

    clean up

--- OpenGL_triangle/cmake-build-release-visual_studio/CMakeFiles/Makefile.cmake
+++ OpenGL_triangle/cmake-build-release-visual_studio/CMakeFiles/Makefile.cmake
@@ -50,5 +50,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/OpenGL_triangle_2.dir/DependInfo.cmake"
+  "CMakeFiles/OpenGL_triangle.dir/DependInfo.cmake"
   )
--- OpenGL_triangle/cmake-build-release-visual_studio/cmake_install.cmake
+++ OpenGL_triangle/cmake-build-release-visual_studio/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: F:/STUDIES/CPP/OpenGL_test/OpenGL_triangle_2
+# Install script for directory: F:/STUDIES/CPP/OpenGL_journey/OpenGL_triangle
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/OpenGL_triangle_2")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/OpenGL_triangle")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "F:/STUDIES/CPP/OpenGL_test/OpenGL_triangle_2/cmake-build-release-visual_studio/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "F:/STUDIES/CPP/OpenGL_journey/OpenGL_triangle/cmake-build-release-visual_studio/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")

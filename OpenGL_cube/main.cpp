4b0bb6104fd5f81ed390f4169d665e6c9116152e
Przemyslaw Markiewicz <kacz.pacz@gmail.com>
2018-12-17 20:13:58 +0100

    colorized cube

--- OpenGL_cube/main.cpp
+++ OpenGL_cube/main.cpp
@@ -38,18 +38,16 @@ int main()
 
 	///tu sie dzieje magia
 
-	/*
+	
 	const color COLOR_YELLOW(255, 255, 0, 255);
 	const color COLOR_BLACK(0, 0, 0, 255);
 	const color COLOR_RED(255, 0, 0, 255);
 	const color COLOR_GREEN(0, 255, 0, 255);
 	const color COLOR_BLUE(0, 0, 255, 255);
-	*/
+	
 
 	cube my_cube;
-
 	my_cube.generate();
-
 	my_cube.render_cube();
 
 
